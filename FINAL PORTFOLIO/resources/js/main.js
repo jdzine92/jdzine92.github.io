a6f1c6936bc47cd34f3b325157e7080f8770a250
Jordan <80704851+jdzine92@users.noreply.github.com>
2022-03-07 16:30:12 +0000

    Add files via upload
    Extensive site redesign

--- FINAL PORTFOLIO/resources/js/main.js
+++ FINAL PORTFOLIO/resources/js/main.js
@@ -1,95 +1,57 @@
-window.onscroll = function() {progressBar()}
+window.onscroll = function () {
+  progressBar();
+};
 
 //PROGRESS SCROLL BAR
 const progressBar = () => {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("scrollBar").style.width = scrolled + "%";
 };
 
-//HUE ROTATOR FOR HEADER/PROFILE + BACK TO TOP BUTTON
-document.querySelector("header").animate(
+//HUE ROTATOR FOR PROGRESS BAR + HEADER/PROFILE + BACK TO TOP BUTTON
+document.getElementById("hcolour").animate(
   {
-    filter: ['hue-rotate(0deg)', 'hue-rotate(360deg)']
+    filter: ["hue-rotate(0deg)", "hue-rotate(360deg)"],
   },
   {
     duration: 60000,
-    iterations: Infinity
+    iterations: Infinity,
   }
 );
 
 document.getElementById("backtotop").animate(
   {
-    filter: ['hue-rotate(0deg)', 'hue-rotate(360deg)']
+    filter: ["hue-rotate(0deg)", "hue-rotate(360deg)"],
   },
   {
     duration: 60000,
-    iterations: Infinity
+    iterations: Infinity,
   }
 );
 
 //EVENT LISTENERS FOR SCROLL AND RESIZE
-window.addEventListener("scroll", function(){
-    if(window.innerWidth > 600) scroller();
-}); 
-
-window.addEventListener("resize", function(){
-    if(window.innerWidth < 600) collapse();
-}); 
-
-window.addEventListener("resize", function(){
-    if(window.innerWidth > 600) expand();
+window.addEventListener("scroll", function () {
+  if (window.innerWidth > 600) scroller();
 });
 
-//COLLAPSE HEADER WHEN SCROLLING
-const scroller = () => {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector("header").style.height = "5rem";
-    document.querySelector("h1").style.display = "none";
-    document.querySelector("nav").style.top = "1rem";
-    document.querySelector("main").style.marginTop = "5rem";
-    document.getElementById("scrollContainer").style.top = "5rem";
-    document.getElementById("scrollBar").style.top = "5rem";  
-    
-  } else {
-    document.querySelector("header").style.height = "15rem";
-    document.querySelector("h1").style.display = "inherit";
-    document.querySelector("nav").style.top = "3rem";
-    document.querySelector("main").style.marginTop = "15rem";
-    document.getElementById("scrollContainer").style.top = "15rem";
-    document.getElementById("scrollBar").style.top = "15rem";   
-  } //end of else 
-}; //end of scroller
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 600) collapse();
+});
 
-
-//COLLAPSE AND EXPAND WINDOW FUNCTIONS (FOR RESIZE)
-const collapse = () => {
-  document.querySelector("header").style.height = "5rem";
-  document.querySelector("h1").style.display = "none";
-  document.querySelector("nav").style.top = "1.4rem";
-  document.querySelector("main").style.marginTop = "5rem";
-  document.getElementById("scrollContainer").style.top = "5rem";
-  document.getElementById("scrollBar").style.top = "5rem";
-};
-
-const expand = () => {
-  if (document.querySelector("header").style.height === "15rem") {
-    document.querySelector("nav").style.top = "3rem";
-  } else {
-    document.querySelector("nav").style.top = "1rem";
-  }
-};
-
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 600) expand();
+});
 
 //BACK TO TOP ONCLICK FUNCTION
-document.getElementById("backtotop").onclick = function() {backToTop()};
+document.getElementById("backtotop").onclick = function () {
+  backToTop();
+};
 
 const backToTop = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
-
-
-
-
